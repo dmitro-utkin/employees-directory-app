f2ce0ea63cdc42b28ac343f4904cbfee885f7143
ДМИТРО <sssddd1366628@gmail.com>
2024-11-06 22:16:38 +0200

    fixed some bugs

--- src/features/EmployeesList/components/EmployeeCard/index.tsx
+++ src/features/EmployeesList/components/EmployeeCard/index.tsx
@@ -1,46 +1,3 @@
-// import React from 'react';
-// import moment from 'moment';
-// import { Employee } from '../../../../common/types';
-
-// import './index.scss';
-
-// type EmployeeCardProps = {
-//   employee: Employee;
-//   showBirthDate: boolean;
-//   onClick: () => void;
-// };
-
-// const EmployeeCard: React.FC<EmployeeCardProps> = ({
-//   employee: { id, birthDate, avatar, name, tag, position },
-//   showBirthDate,
-//   onClick,
-// }) => {
-//   return (
-//     <>
-//       <li className="employee" key={id} onClick={onClick}>
-//         <div className="employee__info">
-//           <div className="employee__image">
-//             <img className="employee__avatar" src={avatar} alt={name} />
-//           </div>
-
-//           <div className="employee__name">
-//             {name}
-//             {tag && <span className="employee__tag">{tag}</span>}
-//             <div className="employee__position">
-//               {position[0].toUpperCase() + position.slice(1)}
-//             </div>
-//           </div>
-//         </div>
-//       </li>
-//       {showBirthDate && (
-//         <div className="employee__birth">{moment(birthDate).format('DD MMMM')}</div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default EmployeeCard;
-
 import React from 'react';
 import moment from 'moment';
 import { Employee } from '../../../../common/types';
@@ -50,9 +7,6 @@ type EmployeeCardProps = {
   employee: Employee;
   showBirthDate: boolean;
   onClick: () => void;
-  sortType: 'alphabet' | 'birthday';
-  index: number;
-  employees: Employee[];
 };
 
 const EmployeeCard: React.FC<EmployeeCardProps> = ({
@@ -70,9 +24,7 @@ const EmployeeCard: React.FC<EmployeeCardProps> = ({
         <div className="employee__name">
           {name}
           {tag && <span className="employee__tag">{tag}</span>}
-          <div className="employee__position">
-            {position[0].toUpperCase() + position.slice(1)}
-          </div>
+          <div className="employee__position">{position[0].toUpperCase() + position.slice(1)}</div>
         </div>
       </div>
       {showBirthDate && (
@@ -83,5 +35,3 @@ const EmployeeCard: React.FC<EmployeeCardProps> = ({
 };
 
 export default EmployeeCard;
-
-
